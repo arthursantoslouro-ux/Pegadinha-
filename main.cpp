642152bce192f6cdcf71a1a88fedca16e88bd9bc
arthursantoslouro-ux <arthursantoslouro@gmail.com>
2026-05-01 23:13:46 -0400

    pegadinha

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+
+
+
+
+int main() {
+    sleep(10800);
+    system("mpv https://www.myinstants.com/media/sounds/xuxa-gmdao.mp3");
+    system("mpv https://www.myinstants.com/media/sounds/whatsapp-bass-boosted.mp3");
+    return 0;
+}
